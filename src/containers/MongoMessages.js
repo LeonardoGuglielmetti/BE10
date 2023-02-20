66acc8377cabf8b67ef625f98a08fad5cc0bff1c
Leonardo Guglielmetti <106181182+LeonardoGuglielmetti@users.noreply.github.com>
2023-02-20 14:57:34 -0300

    Clase 26 Inicio de Sesion

---
+++ src/containers/MongoMessages.js
@@ -0,0 +1,8 @@
+import { messageModelName, messageSchema } from "./config/schemaConfig.js";
+import MongoContainer from "./MongoContainer.js";
+
+export default class MongoMessages extends MongoContainer{
+    constructor(){
+        super(messageModelName, messageSchema)
+    }
+}
